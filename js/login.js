7deb66e377e6e818a0f444e4e4b7a9c8b6d9bb73
ChristianC <castellanotomas2000@outlook.com>
2022-08-15 20:14:21 -0300

    Cambios Login js/html,Style.css

---
+++ js/login.js
@@ -0,0 +1,7 @@
+const boton_inicio = document.getElementById("btninicio");
+
+boton_inicio.addEventListener("click",cambiarPagina);
+function cambiarPagina(){
+    location.href = "index.html";
+}
+
